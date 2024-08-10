76d4d47b671987cb25386758d03bcf828fefedd9
Yuria <deymomanka@gmail.com>
2024-08-11 01:55:19 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -98,7 +98,7 @@ def page_two():
 
     #----------------Percentage of foreign workers in each industry
     st.markdown('### Percentage of foreign workers in each industry:')
-    dfDC['Proportion'].fillna(0, inplace=True)
+    #dfDC['Proportion'].fillna(0, inplace=True)
     dfDC.loc[dfDC['Proportion'] < 2, 'Industry ENG'] = 'Other'
     dfDC = dfDC.groupby('Industry ENG').sum().reset_index()
     fig = go.Figure(data=[go.Pie(labels=dfDC['Industry ENG'], values=dfDC['Proportion'], hole=0.4)])
